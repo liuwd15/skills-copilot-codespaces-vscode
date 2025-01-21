342b704ad1986904be08f7acee607a57ff526c40
Wendao Liu <1045561474@qq.com>
2025-01-21 21:38:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,68 +1,25 @@
 // create web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
-var path = require('path');
-var comments = require('./comments.json');
-var chat = require('./chat.json');
-var server = http.createServer(function(req, res){
-  var url_parts = url.parse(req.url, true);
-  var path = url_parts.pathname;
-  
-  if (path == '/') {
-    fs.readFile('./index.html', function(error, data){
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
-  } else if (path == '/comments.json') {
-    fs.readFile('./comments.json', function(error, data){
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(data);
-    });
-  } else if (path == '/chat.json') {
-    fs.readFile('./chat.json', function(error, data){
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(data);
-    });
-  } else if (path == '/submit') {
-    if (req.method == 'POST') {
-      var body = '';
-      req.on('data', function(data){
-        body += data;
-      });
-      req.on('end', function(){
-        var post = qs.parse(body);
-        comments.push(post);
-        fs.writeFile('./comments.json', JSON.stringify(comments), function(){
-          res.writeHead(302, {'Location': '/'});
-          res.end();
-        });
-      });
-    } else {
-      res.writeHead(405, {'Content-Type': 'text/plain'});
-      res.end('Method Not Allowed');
-    }
-  } else if (path == '/chat') {
-    if (req.method == 'POST') {
-      var body = '';
-      req.on('data', function(data){
-        body += data;
-      });
-      req.on('end', function(){
-        var post = qs.parse(body);
-        chat.push(post);
-        fs.writeFile('./chat.json', JSON.stringify(chat), function(){
-          res.writeHead(302, {'Location': '/'});
-          res.end();
-        });
-      });
-    } else {
-      res.writeHead(405, {'Content-Type': 'text/plain'});
-      res.end('Method Not Allowed');
-    }
-  } else {
-    var filename = path.slice(1);
-    fs.exists(filename, function(exists){
-      if (exists) {
-        fs.readFile(filename, function(error, data){
+// web server listens to port 3000
+// web server has a single endpoint: '/comments'
+// when a GET request is made to '/comments', the server will respond with a JSON object containing an array of comments
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  const comments = [
+    { id: 1, author: 'John Doe', text: 'Hello, world!' },
+    { id: 2, author: 'Jane Doe', text: 'Hi, world!' },
+    { id: 3, author: 'Foo Bar', text: 'Bye, world!' },
+  ];
+
+  res.end(JSON.stringify(comments));
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
